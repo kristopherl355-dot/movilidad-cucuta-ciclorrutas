0555d3d2c03b5b0fd9777b2bb44a18afdd7f6a7e
kristopherl355-dot <kristopherl355@gmail.com>
2026-06-15 18:49:07 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,13 +1,43 @@
-self.addEventListener("install", e=>{
-e.waitUntil(
-caches.open("app").then(cache=>{
-return cache.addAll(["./","index.html"]);
-})
-);
+const CACHE_NAME = "app-v2";
+const APP_FILES = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./ciclorutas.json",
+  "./icon.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(APP_FILES))
+  );
+  self.skipWaiting();
 });
 
-self.addEventListener("fetch", e=>{
-e.respondWith(
-caches.match(e.request).then(r=>r||fetch(e.request))
-);
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  const url = new URL(event.request.url);
+
+  if (url.hostname.includes("script.google.com")) {
+    event.respondWith(fetch(event.request));
+    return;
+  }
+
+  event.respondWith(
+    caches.match(event.request).then(cached => {
+      return cached || fetch(event.request);
+    })
+  );
 });
